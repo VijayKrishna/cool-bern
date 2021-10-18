30c751710c1d8f867b0deb5cd3a44d2ce74c21f7
Vijay Krishna Palepu <vpalepu@uci.edu>
2021-10-17 18:49:57 -0700

    Cool Bern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { Stack, StackItem } from '@fluentui/react/lib/Stack';
+import TweetEmbed from 'react-tweet-embed'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const stackTokens = { childrenGap: 0, padding: 10 };
+var listOfImages = [];
+var listOfTweets = ['1352300537484513282', '1351975936665665537', '1447874068930236416', '1448015441130135552'];
+
+class App extends Component {
+
+  importAll(r) {
+    return r.keys().map(r);
+  }
+  
+  componentWillMount() {
+    listOfImages = this.importAll(require.context('./berns/', false, /\.(png|jpe?g|svg|JPG|webp)$/));
+  }
+
+  render() {
+    return (
+      <Stack vertical tokens={stackTokens}>
+        <StackItem><h1>❄️ Cool Bern</h1></StackItem>
+
+        {
+          listOfImages.map((image, index) => {
+            let number = `${index + 1}.`
+            let alt = `cool bern ${index}`
+            return (
+              <div>
+                <h2>{number}</h2>
+                <img key={index} width="50%" src={image.default} alt={alt} ></img>
+              </div>
+            )
+          })
+        }
+
+        {
+          listOfTweets.map( (tweetId, index) => {
+            let number = `${index + 1 + listOfImages.length}.`
+            return (
+              <div>
+                <h2>{number}</h2>
+                <TweetEmbed id={tweetId} options={{ conversation: 'none' }}/>
+              </div>
+            )
+          })
+        }
+
+
+      </Stack>
+    );
+
+  }
+
 }
 
 export default App;
